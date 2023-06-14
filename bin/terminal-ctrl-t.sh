9931ba27efad48c3a417bf47c552d4a98779fb76
Petr Stepanov <stepanovps@gmail.com>
2023-06-14 11:10:05 -0700

    Updates

--- bin/terminal-ctrl-t.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ new-tab 'disabled'
-xdotool key ctrl+t
-
-sleep 2
-gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ new-tab '<Primary>t'
