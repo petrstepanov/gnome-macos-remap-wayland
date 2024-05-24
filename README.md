7f40e6517d616da05ff904be8b8478132f4e6271
Petr Stepanov <stepanovps@gmail.com>
2024-05-24 00:40:55 -0700

    Updates
    1. Tweaked the install script.
    2. Partial support for GNOME Console
    3. Fixed Ctrl+D for nano editor in GNOME Terminal

--- README.md
+++ README.md
@@ -38,6 +38,8 @@ chmod +x ./uninstall.sh
 
 ## Logbook
 
+`May 24, 2024` • Added keybindings for [GNOME Console](https://apps.gnome.org/Console/) app.
+
 `Oct 01, 2023` • [Blocker issue](https://github.com/k0kubun/xremap/issues/100) is resolved. Repository is ready for testing.
 
 `May 23, 2021` • Moving to [xRemap](https://github.com/k0kubun/xremap) functionality instead.
--- install.sh
+++ install.sh
@@ -5,12 +5,11 @@ BASE_DIR=`pwd`
 mkdir -p ~/Downloads && cd ~/Downloads
 
 # Remove previously downloaded archives (if any)
-rm -rf ./xremap-linux-${ARCH}-*
+rm -rf ./xremap*
 
 # Detect architecture
 ARCH=`uname -m`
-
-echo $ARCH
+echo "INFO: Detected ${ARCH} PC architecture."
 
 # Exit if unsupported architecture
 if [ "${ARCH}" != "x86_64" ] && [ "${ARCH}" != "aarch64" ]; then
@@ -21,23 +20,18 @@ fi
 
 # Detect compositor type - X11 or Wayland
 if [ "${XDG_SESSION_TYPE}" == "x11" ]; then
-  echo "INFO: Detected X11."
+  echo "INFO: Detected X11 compositor."
   ARCHIVE_NAME="xremap-linux-${ARCH}-x11.zip"
 elif [ "${XDG_SESSION_TYPE}" == "wayland" ]; then
-  echo "INFO: Detected Wayland."
+  echo "INFO: Detected Wayland compositor."
   ARCHIVE_NAME="xremap-linux-${ARCH}-gnome.zip"
 else
   echo "ERROR: Unsupported compositor."
-  exit 0
+  exit 1
 fi
 
-# Download corresponding archive
-echo "INFO: Downloading the \"$ARCHIVE_NAME\"..."
-curl -sL https://api.github.com/repos/k0kubun/xremap/releases/latest \
-| grep $ARCHIVE_NAME \
-| cut -d : -f 2,3 \
-| tr -d \" \
-| wget -qi -
+# Download latest release from GitHub - latest 0.10.0 throws `application-client: GNOME (supported: false)`
+wget https://github.com/xremap/xremap/releases/latest/download/$ARCHIVE_NAME
 
 # Extract the archive and install binary to ~/.local/bin
 echo "INFO: Extracting the archive..."
@@ -47,8 +41,14 @@ if ! command -v unzip &> /dev/null; then
 fi
 unzip -o ./xremap-linux-${ARCH}-*.zip
 
+# Remove old binary (if any)
+if command -v gnome-terminal &> /dev/null ; then
+    echo "INFO: Removing old binary..."
+    sudo rm -rf /usr/local/bin/xremap
+fi
+
+# Install new binary (if any)
 echo "INFO: Installing the binary..."
-# sudo systemctl stop gnome-macos-remap
 sudo cp ./xremap /usr/local/bin
 
 # Tweaking server access control for X11 https://github.com/k0kubun/xremap#x11
@@ -58,15 +58,21 @@ fi
 
 # Copy xremap config file with macOS bindings
 CONFIG_DIR=~/.config/gnome-macos-remap/
-echo "INFO: Copying the xremap config file to ${CONFIG_DIR}"
+echo "INFO: Copying the xremap config file..."
 mkdir -p $CONFIG_DIR
 cp $BASE_DIR/config.yml $CONFIG_DIR
 
 # Copy systemd service file
 SERVICE_DIR=~/.local/share/systemd/user/
-echo "INFO: Installing systemd service to ${SERVICE_DIR}"
+echo "INFO: Installing systemd service..."
 mkdir -p $SERVICE_DIR
 cp $BASE_DIR/gnome-macos-remap.service $SERVICE_DIR
+
+# Copy bash scripts
+BIN_DIR=~/.local/bin/
+echo "INFO: Copying bash scripts..."
+mkdir -p $BIN_DIR
+cp $BASE_DIR/bin/*.sh $BIN_DIR
 
 # Run xremap without sudo
 sudo gpasswd -a ${USER} input
@@ -76,14 +82,6 @@ echo 'KERNEL=="uinput", GROUP="input", TAG+="uaccess"' | sudo tee /etc/udev/rule
 systemctl --user daemon-reload
 systemctl --user enable gnome-macos-remap
 systemctl --user start gnome-macos-remap
-
-# Download and enable xremap GNOME extension (for Wayland only)
-if [ "${XDG_SESSION_TYPE}" == "wayland" ]; then
-  echo "INFO: Action Required. Please install the xremap extension."
-  echo "      https://extensions.gnome.org/extension/5060/xremap/"
-else
-  gnome-extensions disable xremap@k0kubun.com
-fi
 
 # Tweak gsettings
 echo "INFO: Tweaking GNOME and Mutter keybindings..."
@@ -129,13 +127,16 @@ gsettings set org.gnome.shell.keybindings toggle-overview "['LaunchA']"
 gsettings set org.gnome.shell.keybindings toggle-application-view "['<Primary>space', 'LaunchB']"
 
 # Setting relocatable schema for Terminal
-gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ copy '<Shift><Super>c'
-gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ paste '<Shift><Super>v'
-gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ new-tab '<Shift><Super>t'
-gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ new-window '<Shift><Super>n'
-gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ close-tab '<Shift><Super>w'
-gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ close-window '<Shift><Super>q'
-gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ find '<Shift><Super>f'
+if command -v gnome-terminal &> /dev/null ; then
+    echo "INFO: Found GNOME Terminal. Applying tweaks..."
+    gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ copy '<Shift><Super>c'
+    gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ paste '<Shift><Super>v'
+    gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ new-tab '<Shift><Super>t'
+    gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ new-window '<Shift><Super>n'
+    gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ close-tab '<Shift><Super>w'
+    gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ close-window '<Shift><Super>q'
+    gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ find '<Shift><Super>f'
+fi
 
 # Screenshots
 # gsettings set org.gnome.shell.keybindings screenshot "['<Primary><Shift>numbersign']"
@@ -151,4 +152,14 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys screensaver "[]"
 # Restart is required in order for the changes in the `/usr/share/dbus-1/session.conf` to take place
 # Therefore cannot launch service right away
 
-echo "INFO: Done. Please restart your computer."
+
+# Download and enable xremap GNOME extension (for Wayland only)
+if [ "${XDG_SESSION_TYPE}" == "wayland" ]; then
+  RED=`tput setaf 1`
+  RESET=`tput sgr0`
+  echo "INFO: ${RED}Action Required${RESET}. Install the xremap extension and restart your PC."
+  echo "      https://extensions.gnome.org/extension/5060/xremap/"
+else
+  gnome-extensions disable xremap@k0kubun.com
+  echo "INFO: Please restart your computer."
+fi
